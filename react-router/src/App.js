e7597912ea135e74fa545b53b237d7d7215733fe
Ananya015 <jainananya78@gmail.com>
2021-04-11 00:10:27 +0530

    fixing

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Home from './pages/home';
+import Contact from './pages/contact';
+import About from './pages/about';
 
 function App() {
   return (
     <div className="App">
-    
+    <Home/>
+    <Contact/>
+    <About/>
     </div>
   );
 }
