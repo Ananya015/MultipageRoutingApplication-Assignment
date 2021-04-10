c33c04d9d4e0875cfa2c5b7bb9e481977ff3e02a
Ananya015 <jainananya78@gmail.com>
2021-04-11 00:35:45 +0530

    adding navbar

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import './App.css';
-import Home from './pages/home';
-import Contact from './pages/contact';
-import About from './pages/about';
+// import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Navbar from './pages/Navbar';
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
     <Home/>
     <Contact/>
     <About/>
