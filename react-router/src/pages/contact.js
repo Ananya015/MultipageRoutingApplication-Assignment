5047ef94f7f67eb3259b1cd01f1dcd89c3397dad
Ananya015 <jainananya78@gmail.com>
2021-04-11 12:53:31 +0530

    adding content to pages

--- react-router/src/pages/contact.js
+++ react-router/src/pages/contact.js
@@ -4,6 +4,20 @@ export default function Contact(){
     return(
         <div>
             <h1>Contact Us</h1>
+            <p>Most contact pages are designed with function in mind.
+
+They slap an email address, phone, and location on a plain background and call it a day.
+
+But basic contact pages don’t inspire visitors to reach out and connect.
+
+Other pages make it easy to contact the company – which is awesome.
+
+Except, that can also drive up customer service costs.
+
+So what makes the perfect Contact Us page?
+
+An awesome Contact Us page finds just the right balance between making it easy to reach the company and sharing resources users can use to answer their questions right away.
+            </p>
         </div>
     )
 }
