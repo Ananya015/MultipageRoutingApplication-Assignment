2b432d898d01072655f7f35467f6ffefe47dae83
Ananya015 <jainananya78@gmail.com>
2021-04-11 12:47:32 +0530

    adding new page

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react'
-// import './App.css';
+import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Navbar from './pages/Navbar';
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
   return (
+    <Router>
     <div className="App">
     <Navbar />
-    <Home/>
-    <Contact/>
-    <About/>
+    <Switch>
+      <Route exact path="/"component={Home}></Route>
+      <Route exact path="/about"component={About}></Route>
+      <Route exact path="/contact"component={Contact}></Route>
+      <Route component={PageNotFound}></Route>
+    </Switch>
+   
     </div>
+    </Router>
   );
 }
 
--- react-router/src/pages/Navbar.js
+++ react-router/src/pages/Navbar.js
@@ -1,35 +1,24 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return(
         <div>
-           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+           <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <Link class="navbar-brand" to="#">React Linking</Link>
+  
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <Link class="nav-link" to="/about">About</Link>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+     
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <Link class="nav-link" to="/contact">Contact</Link>
       </li>
     </ul>
     
